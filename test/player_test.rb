eff3f51883e1c1375b4065bf82ce5bb96e050dc8
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-04-18 20:26:38 -0400

    :sparkles: Add player_test.rb and player.rb
    Add player_test.rb and player.rb

---
+++ test/player_test.rb
@@ -0,0 +1,68 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/player'
+
+class DeckTest < Minitest::Test
+  def test_it_exists
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    player = Player.new('Clarisa', deck)
+
+    assert_instance_of Player, player
+  end
+
+  def test_player_has_a_name
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    player = Player.new('Clarisa', deck)
+
+    assert_equal 'Clarisa', player.name
+  end
+
+  def test_player_has_a_deck
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    player = Player.new('Clarisa', deck)
+
+    assert_equal [card1, card2, card3], player.deck.cards
+  end
+
+  def test_default_player_has_not_lost
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    player = Player.new('Clarisa', deck)
+
+    assert_equal false, player.has_lost?
+  end
+
+  def test_if_deck_has_no_cards_player_loses
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    player = Player.new('Clarisa', deck)
+    assert_equal false, player.has_lost?
+
+    player.deck.remove_card
+    player.deck.remove_card
+    assert_equal false, player.has_lost?
+
+    player.deck.remove_card
+    assert_equal true, player.has_lost?
+    assert_equal [], player.deck.cards
+  end
+
+end
