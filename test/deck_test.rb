4255a5fdc89b241f76dcca5eedebbfb5a1fd5672
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-04-19 08:28:41 -0400

    :art: Add setup method to DeckTest
    Add setup method to DeckTest

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -4,76 +4,43 @@ require './lib/card'
 require './lib/deck'
 
 class DeckTest < Minitest::Test
-  def test_it_exists
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
+  def setup
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
+    @cards = [@card1, @card2, @card3]
+    @deck = Deck.new(@cards)
+  end
 
-    assert_instance_of Deck, deck
+  def test_it_exists
+    assert_instance_of Deck, @deck
   end
 
   def test_it_can_take_cards
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-
-    assert_equal [card1, card2, card3], deck.cards
+    assert_equal [@card1, @card2, @card3], @deck.cards
   end
 
   def test_return_rank_of_card_at_index
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-
-    assert_equal 12, deck.rank_of_card_at(0)
+    assert_equal 12, @deck.rank_of_card_at(0)
   end
 
   def test_return_array_of_high_ranking_cards
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-
-    assert_equal [card1, card3], deck.high_ranking_cards
+    assert_equal [@card1, @card3], @deck.high_ranking_cards
   end
 
   def test_return_percent_high_ranking_cards
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-
-    assert_equal 66.67, deck.percent_high_ranking
+    assert_equal 66.67, @deck.percent_high_ranking
   end
 
   def test_can_remove_card
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
-
-    assert_equal card1, deck.remove_card
-    assert_equal [card2, card3], deck.cards
+    assert_equal @card1, @deck.remove_card
+    assert_equal [@card2, @card3], @deck.cards
   end
 
   def test_can_add_card
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    cards = [card1, card2, card3]
-    deck = Deck.new(cards)
     card4 = Card.new(:club, '5', 5)
-    deck.add_card(card4)
+    @deck.add_card(card4)
 
-    assert_equal [card1, card2, card3, card4], deck.cards
+    assert_equal [@card1, @card2, @card3, card4], @deck.cards
   end
 end
