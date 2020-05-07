779612d249037f7f593d81ed7bc211b347e04e8c
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-05-07 16:20:20 -0400

    :sparkles: Add additional tests to start_test
    Add additional tests to start_test

--- test/start_test.rb
+++ test/start_test.rb
@@ -7,32 +7,131 @@ require './lib/turn'
 require './lib/start'
 
 class StartTest < MiniTest::Test
-  def setup
+  def test_it_exists
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
-    card3 = Card.new(:heart, '9', 9)
-    card4 = Card.new(:diamond, 'Jack', 11)
-    card5 = Card.new(:heart, '8', 8)
-    card6 = Card.new(:diamond, 'Queen', 12)
-    card7 = Card.new(:heart, '3', 3)
-    card8 = Card.new(:diamond, '2', 2)
 
-    deck1 = Deck.new([card1, card2, card3, card4])
-    deck2 = Deck.new([card5, card6, card7, card8])
+    deck1 = Deck.new([card1])
+    deck2 = Deck.new([card2])
 
     player1 = Player.new("Jessye", deck1)
     player2 = Player.new("Lynne", deck2)
 
-    @start = Start.new(player1, player2)
+    start = Start.new(player1, player2)
+
+    assert_instance_of Start, start
   end
 
-  def test_it_exists
-    setup
+  def test_it_has_two_players
+    # skip
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
 
-    assert_instance_of Start, @start
+    deck1 = Deck.new([card1])
+    deck2 = Deck.new([card2])
+
+    player1 = Player.new("Jessye", deck1)
+    player2 = Player.new("Lynne", deck2)
+
+    start = Start.new(player1, player2)
+
+    assert_equal "Jessye", start.player1.name
+    assert_equal "Lynne", start.player2.name
   end
 
-  def test
+  def test_turn_num_starts_at_1
+    # skip
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+
+    deck1 = Deck.new([card1])
+    deck2 = Deck.new([card2])
+
+    player1 = Player.new("Jessye", deck1)
+    player2 = Player.new("Lynne", deck2)
+
+    start = Start.new(player1, player2)
+
+    assert_equal 1, start.turn_num
+  end
+
+  def test_final_winner_starts_empty
+    # skip
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+
+    deck1 = Deck.new([card1])
+    deck2 = Deck.new([card2])
+
+    player1 = Player.new("Jessye", deck1)
+    player2 = Player.new("Lynne", deck2)
+
+    start = Start.new(player1, player2)
+
+    assert_equal '', start.final_winner
+  end
+
+  def test_play_method
+    skip
+    # Need to figure out how to do this
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+
+    deck1 = Deck.new([card1])
+    deck2 = Deck.new([card2])
+
+    player1 = Player.new("Jessye", deck1)
+    player2 = Player.new("Lynne", deck2)
+
+    start = Start.new(player1, player2)
+  end
+
+  def test_play_outputs_welcome
+    skip
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+
+    deck1 = Deck.new([card1])
+    deck2 = Deck.new([card2])
+
+    player1 = Player.new("Jessye", deck1)
+    player2 = Player.new("Lynne", deck2)
+
+    start = Start.new(player1, player2)
+    assert_output("Welcome to War! (or Peace) This game will be played with 52 cards.", "o") do
+      start.play
+    end
+  end
+
+  def test_turn_creates_instance_of_turn_class
+    skip
+  end
+
+  def test_basic_turn_if_turn_type_is_basic
+    skip
+  end
+
+  def test_war_turn_if_turn_type_is_war
+    skip
+  end
+
+  def test_mad_turn_if_turn_type_is_mad
+    skip
+  end
+
+  def test_turn_num_accumulates_each_turn
+    skip
+  end
+
+  def test_turn_loop_breaks_if_player_lost
+    skip
+  end
+
+  def test_turn_loop_breaks_if_turn_num_is_1000000
+    skip
+  end
+
+  def test_it_displays_final_winner
     skip
   end
 end
